6eb75bac73d78d16e675834fb90015e25c3478fc
aomine <xomarxpl07@outlook.es>
2019-03-14 20:26:46 -0500

    v 1.2

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
 const hamburgerBox = document.querySelector('.hamburger-box'),
 sidenavOverlay = document.querySelector('.sidenav-overlay'),
+links = document.querySelectorAll('.link'),
 ipad = window.matchMedia('screen and (max-width: 767.8px)');
 
 function validation(e) {  //const validation = e => {
 	if (e.matches) {
 		hamburgerBox.addEventListener('click', addActive);
 		sidenavOverlay.addEventListener('click',remove);
+		for(let i of links){
+			i.addEventListener('click',remove);
+		}
 	}else{
 		hamburgerBox.removeEventListener('click', addActive);
 		sidenavOverlay.removeEventListener('click',remove);
+		for(let i of links){
+			i.removeEventListener('click',remove);
+		}
 	}
 }
 
